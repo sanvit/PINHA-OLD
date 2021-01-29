3f04a090c2720e4bb2fd6afa402f2dacf450af2e
numeru <cyk000418@naver.com>
2021-01-29 19:13:17 +0900

    Header, Login, Register 컴포넌트 추가

---
+++ frontend/src/components/Layout/Header.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "components/Layout/Header.scss";
+import { useAppContext } from "stores/jwt-store";
+import { useHistory } from "react-router-dom";
+
+const Header = () => {
+  const history = useHistory();
+
+  const [isMainView, setIsMainView] = useState(true);
+
+  const {
+    state: { isAuthenticated },
+  } = useAppContext();
+
+  const goToPage = (e, path) => {
+    history.push({
+      pathname: `${path}`,
+      state: { view: e.target.name },
+    });
+    setIsMainView(false);
+  };
+
+  const goToHome = () => {
+    history.push("/");
+    setIsMainView(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="header">
+      <img src="" alt="logo" className="header-logo" onClick={goToHome} />
+      <form className="header-search" onSubmit={(e) => handleSubmit(e)}>
+        {isMainView && (
+          <input
+            className="header-input"
+            placeholder="원하는 가게를 검색해보세요."
+          />
+        )}
+      </form>
+      <div className="header-buttonList">
+        {!isAuthenticated ? (
+          <>
+            <button
+              name="login"
+              className="header-button login"
+              onClick={(e) => goToPage(e, "Auth")}
+            >
+              로그인
+            </button>
+            <button
+              name="register"
+              className="header-button register"
+              onClick={(e) => goToPage(e, "Auth")}
+            >
+              회원가입
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              name="mypage"
+              className="header-button mypage"
+              onClick={(e) => goToPage(e, "MyPage")}
+            >
+              마이페이지
+            </button>
+            <button className="header-button logout">로그아웃</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;

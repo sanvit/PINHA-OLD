49db0128e9facbea17499b032e837419a28af1d3
amamov <ysangsuk78@gmail.com>
2021-01-29 22:21:07 +0900

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import AppRouter from "AppRouter";
 import { AppProvider as JWTProvider } from "stores/jwt-store";
-import { RenderAfterNavermapsLoaded } from "react-naver-maps";
 
 function App() {
   return (
-    <RenderAfterNavermapsLoaded
-      ncpClientId={process.env.REACT_APP_NCP_CLIENT_ID}
-      error={<p>Maps Load Error</p>}
-      loading={<p>Maps Loading...</p>}
-    >
-      <JWTProvider>
-        <AppRouter />
-      </JWTProvider>
-    </RenderAfterNavermapsLoaded>
+    <JWTProvider>
+      <AppRouter />
+    </JWTProvider>
   );
 }
 

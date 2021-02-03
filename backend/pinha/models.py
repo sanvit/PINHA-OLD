be68805f6ea1ff5ff2eb8fe63cff962c15257014
sanvit <sanvit@sanvit.me>
2021-02-03 04:45:26 +0000

    add django models

--- backend/pinha/models.py
+++ backend/pinha/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.conf import settings
+import uuid
+from django.utils import timezone
 
 # Create your models here.
+
+
+def image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    id = str(uuid.uuid4())
+    filename = f"{id[:2]}/{id[2:]}.{ext}"
+    return f"images/{filename}"
+
+
+class PhoneAuth(models.Model):
+    number = models.CharField(max_length=11, null=False, blank=False, unique=True)
+    code = models.CharField(max_length=6, null=False, blank=False, unique=False)
+    sentTime = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        self.sentTime = timezone.now()
+
+    def __str__(self):
+        return self.number
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    time = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    address = models.CharField(max_length=100, null=False, blank=False)
+    kakao_id = models.CharField(max_length=50, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    stars = models.DecimalField(max_digits=3, decimal_places=2, null=False, blank=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    modifiedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='reviews')
+    title = models.CharField(max_length=100, null=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    contents = models.TextField()
+    stars = models.PositiveSmallIntegerField(null=False, blank=False)
+    is_public = models.BooleanField(default=True, null=False, blank=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    modifiedAt = models.DateTimeField(auto_now=True)
+    liked = models.PositiveSmallIntegerField(null=False, blank=False, default=0)
+    disliked = models.PositiveSmallIntegerField(null=False, blank=False, default=0)
+
+    def save(self, *args, **kwargs):
+        reviews = self.store.reviews.all()
+        stars = 0
+        for review in reviews:
+            stars += review.stars
+        stars = stars / len(reviews)
+        self.store.stars = stars
+        self.store.save()
+
+    def __str__(self):
+        return f"{self.store.name} {self.title}"
+
+
+class Menu(models.Model):
+    image = models.ImageField(upload_to=image_path, null=False, blank=False)
+    page = models.PositiveSmallIntegerField(default=1, null=False, blank=False)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.store.name} {self.page}"
+
+
+class Photo(models.Model):
+    caption = models.CharField(max_length=100, null=True, blank=True)
+    file = models.ImageField(upload_to=image_path, null=False, blank=False)
+    review = models.ForeignKey(Review, null=True, blank=True, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, null=False, blank=False, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    modifiedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.store.name} {self.store.primary_key}"
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
+from pinha.models import Store, image_path
+import uuid
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, phoneNumber, nickname, password):
+        if not phoneNumber:
+            raise ValueError("Phone Number is Required")
+        user = self.model(
+            phoneNumber=phoneNumber,
+            nickname=nickname
+        )
+        if password:
+            user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phoneNumber, nickname, password):
+        user = self.model(
+            nickname=nickname,
+            phoneNumber=phoneNumber
+        )
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    objects = UserManager()
+    nickname = models.CharField(
+        max_length=20, null=False, blank=False, unique=True)
+    phoneNumber = models.CharField(
+        max_length=11, null=False, blank=False, unique=True)
+    avatar = models.ImageField(upload_to=image_path, null=True, blank=True)
+    background = models.ImageField(upload_to=image_path, null=True, blank=True)
+    email = models.EmailField(
+        max_length=255, unique=True, null=True, blank=True)
+    is_school_verified = models.BooleanField(
+        default=False, null=False, blank=False)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(default=uuid.uuid4(), null=False, blank=False)
+
+    USERNAME_FIELD = 'phoneNumber'
+    REQUIRED_FIELDS = ['nickname']
+
+
+class Badge(models.Model):
+    is_first_avatar_change = models.BooleanField(
+        default=False, blank=False, null=False)
+    is_first_review = models.BooleanField(
+        default=False, blank=False, null=False)
+    shared_count = models.PositiveSmallIntegerField(
+        default=0, blank=False, null=False)
+    is_first_save_store = models.BooleanField(
+        default=False, blank=False, null=False)
+    review_count = models.PositiveSmallIntegerField(
+        default=0, blank=False, null=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             null=False, blank=False, on_delete=models.CASCADE)
+
+
+class FavList(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='favlist', null=False)
+    store = models.ManyToManyField(Store)
+    caption = models.CharField(max_length=100, null=True, blank=True)
+    is_public = models.BooleanField(default=True, blank=False, null=False)

1c77c604226780e270b3d3791360dcaaaa863d33
sanvit <sanvit@sanvit.me>
2021-02-03 05:39:07 +0000

    add kakao api for searching

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,7 +11,8 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
     import debug_toolbar
 
--- backend/pinha/views.py
+++ backend/pinha/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import requests
 # Create your views here.
+
+
+
+@csrf_exempt
+def getMarketList(req):
+    if not req.POST.get('query'):
+        return JsonResponse({'status': 'ERROR', 'message': '검색어를 입력해주세요.'})
+    query = req.POST.get('query')
+    queryString = {'query': query, 'page': 1, 'size': 15, 'sort': 'accuracy',
+                   'x': 126.651542258118, 'y': 37.4514480321002, 'radius': 1500}
+    if req.POST.get('type'):
+        queryString['category_group_code'] = req.POST.get('type')
+    else:
+        queryString['category_group_code'] = 'FD6'
+    url = "https://dapi.kakao.com/v2/local/search/keyword.json"
+    read = requests.get(url, params=queryString, headers={
+                        'Authorization': f"KakaoAK {KAKAO_API_KEY}"})
+    storeList = read.json()['documents']
+    print(storeList)
+    storeListParse = []
+    b = {}
+    for a in storeList:
+        b['name'] = a['place_name']
+        b['address'] = a['road_address_name']
+        b['id'] = a['id']
+        b['phone'] = a['phone']
+        b['category'] = a['category_name']
+        storeListParse.append(b.copy())
+    return JsonResponse({'count':len(storeListParse),'list': storeListParse}, json_dumps_params={'ensure_ascii': False})

0745d3cf8bb683f5b3325634c54a05e38c182e0c
amamov <ysangsuk78@gmail.com>
2021-02-03 14:35:30 +0900

    create openNotificationWithIcon utils func by amam

--- frontend/src/components/feature/Auth/Login.jsx
+++ frontend/src/components/feature/Auth/Login.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "antd";
+import openNotification from "utils/openNotification";
 import "components/feature/Auth/Login.scss";
+
 const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+
   return (
     <form className="login-form" onSubmit={(e) => handleSubmit(e)}>
       <span className="login-title">LogIn</span>
@@ -11,6 +15,7 @@ const Login = () => {
       <button type="submit" className="login-button">
         OKAY
       </button>
+      <Button onClick={() => openNotification()}>Success</Button>
     </form>
   );
 };
